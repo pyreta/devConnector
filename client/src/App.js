d2a5d8659b69fee44014f9de86277b8ef321dd74
Louis Abramson <pyreta@gmail.com>
2019-10-20 15:08:31 -0400

    add register and login routes and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
-<React.Fragment>
-  <Navbar />
-  <Landing />
-</React.Fragment>
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </React.Fragment>
+  </Router>
 );
 
 export default App;
